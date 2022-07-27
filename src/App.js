57f98cc56c7abb3be83d1202de15bf26b77082a7
sehyeon <hyeon4687@naver.com>
2022-07-27 19:36:55 +0900

    푸터 재수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from './components/Main';
 import Nav from './components/Nav';
-import Footer from './components/Footer';
 import Jeju from './pages/Jeju';
 import FAQ from './pages/FAQ';
 import "./App.css";
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,6 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute;
-  bottom: 0;
 `;
 
 const Footer = () => {
